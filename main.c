b649c15479ae6b8605b03da59d39b6a9d0ebafa7
Benjamin Lee <blee@F3C027.42adel.org.au>
2021-06-26 12:59:29 +0930

    feat: created a prototype of the ft_genX function

--- main.c
+++ main.c
@@ -1,8 +1,40 @@
 #include <unistd.h>
-#include <stdio.h>
+
+void rush(int x, int y);
+void ft_putchar(char ch);
+void ft_genX(int x);
+void ft_genY(int y);
 
 int	main(void)
 {
-	rush(123, 42);
+	rush(5, 5);
 	return (0);
 }
+
+void ft_putchar(char ch)
+{
+	write(1, &ch, 1);
+}
+
+void rush(int x, int y)
+{
+	ft_genX(x);
+}
+
+void ft_genX(int x)
+{
+	int h;
+	int c;
+	c = 0;
+	h = x -2;
+	ft_putchar(111);
+
+	while (c < h)
+	{
+		ft_putchar(45);
+		c++;
+	}
+	ft_putchar(111);
+	ft_putchar(10);
+}
+
